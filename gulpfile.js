a357202fac9deb85ec51f190d97317cb9616c208
J.Mejia <james@thirdandgrove.com>
2016-01-14 18:24:33 -0600

    Updated gulpfile css minify task with new cssnano.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
   sass = require('gulp-sass'),
   autoprefixer = require('gulp-autoprefixer'),
-  minifycss = require('gulp-minify-css'),
+  cssnano = require('gulp-cssnano'),
   jshint = require('gulp-jshint'),
   uglify = require('gulp-uglify'),
   rename = require('gulp-rename'),
@@ -22,7 +22,7 @@ gulp.task('styles', function() {
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest(dirs.dist + '/css'))
     .pipe(rename({suffix: '.min'}))
-    .pipe(minifycss())
+    .pipe(cssnano())
     .pipe(gulp.dest(dirs.dist + '/css'))
     .pipe(notify({ message: 'Styles task complete' }))
 });
