4a4dfa892e0d9528712d7a2c0a27a3e853e75a70
J.Mejia <james@thirdandgrove.com>
2016-01-15 11:04:46 -0600

    Update gulpfile to return scss directory and pipe sass() vs just returning sass.

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,8 @@ var pkg = require('./package.json');
 var dirs = pkg['gbp-configs'].directories;
 
 gulp.task('styles', function() {
-  return sass(dirs.lib + '/scss/', { style: 'expanded' })
+  return gulp.src(dirs.lib + '/scss/**/*.scss')
+    .pipe(sass({ style: 'expanded' }))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest(dirs.dist + '/css'))
     .pipe(rename({suffix: '.min'}))
